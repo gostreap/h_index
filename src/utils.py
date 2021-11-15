8c7696237c80846444c437a3af5c1fe7f254dd82
Tristan François <tpv.francois@gmail.com>
2021-11-15 16:35:41 +0100

    Add utils function in order to convert abstract to text

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+def get_abstract_text(abstract):
+    """If abstracts is the dictionnary return by read_data.get_abstracts() then this function take as input abstracts[paper_id]
+
+    Returns:
+        string: the text of the abstract
+    """
+    length = abstract["IndexLength"]
+    text_tab = [None for _ in range(length)]
+    for word, pos in abstract["InvertedIndex"].items():
+        for i in pos:
+            text_tab[i] = word
+    return " ".join(text_tab)
