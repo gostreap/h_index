b3ea1a9be65099b6626f87099c8f6418a58b82c1
michael fotso <michael.fotso@alphabody.fr>
2021-11-18 23:17:22 +0100

    restoration

--- src/submit.py
+++ src/submit.py
@@ -27,7 +27,7 @@ def submit(model):
     train_path = "../tmp/train.txt"
     df_to_txt(train,train_path)
 
-    model_fasttext = fasttext.train_supervised(train_path,lr = 0.626905, dim = 12, epoch = 11, wordNgrams =3)
+    model_fasttext = fasttext.train_supervised(train_path,lr = 0.15815, dim = 2, epoch = 33, wordNgrams =3)
 
     train_core_number = get_core_number(train["author"])
     test_core_number = get_core_number(test["author"])
@@ -43,6 +43,7 @@ def submit(model):
 
     X_train,y_train = format_data(train, model_fasttext, train_core_number, train_min_coauthor_hindex, train_max_coauthor_hindex, train_pagerank)
     X_test, y_test = format_data(test, model_fasttext, test_core_number, test_min_coauthor_hindex, test_max_coauthor_hindex, test_pagerank)
+    
 
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
