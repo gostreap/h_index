fd69dcb234044164f228e43f57cf2f44dfba87c5
Tristan François <tpv.francois@gmail.com>
2021-11-19 00:11:38 +0100

    Store whole dataset in one file

--- src/fasttext_utils.py
+++ src/fasttext_utils.py
@@ -1,33 +1,67 @@
 import csv
 import json
-from networkx.algorithms.link_analysis.pagerank_alg import pagerank
-import pandas as pd
+import os
+
 import numpy as np
+import pandas as pd
+from gensim.utils import simple_preprocess
+from networkx.algorithms.link_analysis.pagerank_alg import pagerank
 from sklearn.cluster import KMeans
 
-from gensim.utils import simple_preprocess
-from read_data import get_graph, get_train_data_json
+from read_data import get_graph, get_test_data, get_train_data, get_train_data_json
+from utils import (
+    get_abstract_text,
+    get_all_coauthors_mean_hindex,
+    get_all_number_of_coauthors,
+)
 
-from utils import get_abstract_text, get_all_coauthors_mean_hindex, get_all_number_of_coauthors
 
-def store_whole_dataset(train, path="../tmp/data_part"):
-    # Store all data
-    flag = True
+TRAIN_FULL_PATH = "../tmp/train_full.csv"
+TEST_FULL_PATH = "../tmp/test_full.csv"
+
+
+def add_features(data, new_features):
+    pass
+
+def store_full_dataset_with_features():
+    train, _ = get_train_data()
+    test, _ = get_test_data()
+
+    # store_whole_dataset(train, "../tmp/train")
+    store_whole_dataset(test, "../tmp/test")
+
+
+def store_whole_dataset(data: pd.DataFrame, path: str):
+    """
+    Launch preprocessing_for_fastText by chunk of 10000 on the wall dataset passed in argument and stores it in a file.
+
+    Args:
+        data (pd.DataFrame): [description]
+        path (string): [description]
+    """
     start = 0
-    end = 0
-    end = 10000 if end <= train.shape[0] else train.shape[0]
+    end = 10000
     i = 1
-    while(end<= train.shape[0] and start< train.shape[0]):
-        if not end < train.shape[0]:
-            print(end+1)
-            temp_data = preprocessing_for_fastText(0,train,start,end+1)
+    while start < data.shape[0]:
+        if not end < data.shape[0]:
+            print(end + 1)
+            temp_data = preprocessing_for_fasttext(0, data, start, end + 1)
         else:
             print(end)
-            temp_data = preprocessing_for_fastText(0,train,start,end)
-        temp_data.to_csv(path+str(i)+".csv",index = None)
+            temp_data = preprocessing_for_fasttext(0, data, start, end)
+        temp_data.to_csv(path + str(i) + ".csv", index=None)
         start = end
-        end = end+10000 if end+10000 <= train.shape[0] else train.shape[0]
-        i = i+1
+        end = end + 10000 if end + 10000 <= data.shape[0] else data.shape[0]
+        i = i + 1
+
+    data_parts = []
+    for i in range(1, i):
+        part_path = path + str(i) + ".csv"
+        data_parts.append(pd.read_csv(part_path))
+        os.remove(part_path)
+    data = pd.concat(data_parts)
+    data.to_csv(path + "_full.csv")
+
 
 def df_to_txt(data, file_name):
     data[["hindex_lab", "text"]].to_csv(
@@ -69,22 +103,22 @@ def get_authors_id_by_papers_id_dict(ids):
 
 
 def small_class(data, k):
-    index = np.sort(np.array(data['hindex'].to_list())).reshape(-1,1)
-    clusters = KMeans(n_clusters=k,random_state=1).fit(index)
+    index = np.sort(np.array(data["hindex"].to_list())).reshape(-1, 1)
+    clusters = KMeans(n_clusters=k, random_state=1).fit(index)
     data["modindx"] = data["hindex"].apply(lambda x: clusters.predict([[x]])[0])
     data["hindex_lab"] = data["modindx"].apply(lambda x: "__label__" + str(x))
     return data
 
 
-def preprocessing_for_fastText(n_sample, data,start=0,end=0):
+def preprocessing_for_fasttext(n_sample, data, start=0, end=0):
     # get a random subset of data
-    if (end>start):
-        sample_data = data.iloc[start:end,:]
+    if end > start:
+        sample_data = data.iloc[start:end, :]
     else:
         sample_data = data.sample(n=n_sample, random_state=1)
     author_ids = sample_data["author"].to_list()
     paper_id_to_author_id = get_authors_id_by_papers_id_dict(author_ids)
-    
+
     abstract = get_all_text_by_author_id(paper_id_to_author_id)
     abstract_text_by_author_id = {}
     for author_id in abstract.keys():
@@ -98,8 +132,8 @@ def preprocessing_for_fastText(n_sample, data,start=0,end=0):
         ]
 
     for author_id in set(author_ids).difference(set(abstract.keys())):
-        abstract_text_by_author_id[author_id]=["", 0]
-    
+        abstract_text_by_author_id[author_id] = ["", 0]
+
     df_abstract_text = pd.DataFrame.from_dict(
         abstract_text_by_author_id, orient="index", columns=["text", "nb_paper"]
     )
@@ -116,8 +150,14 @@ def preprocessing_for_fastText(n_sample, data,start=0,end=0):
     n_coauthors = get_all_number_of_coauthors(author_ids, G, train_data_json)
 
     # TODO : change 9.841160 by mean value of hindex of author
-    df_data["mean_coauthors_hindex"] = df_data["author"].apply(lambda author_id: coauthors_hindex[author_id] if coauthors_hindex[author_id] is not None else 9.841160)
-    df_data["n_coauthors"] = df_data["author"].apply(lambda author_id: n_coauthors[author_id])
+    df_data["mean_coauthors_hindex"] = df_data["author"].apply(
+        lambda author_id: coauthors_hindex[author_id]
+        if coauthors_hindex[author_id] is not None
+        else 9.841160
+    )
+    df_data["n_coauthors"] = df_data["author"].apply(
+        lambda author_id: n_coauthors[author_id]
+    )
 
     return df_data
 
@@ -135,17 +175,47 @@ def general_comp(model, test):
     return test_err
 
 
-def format_data(data, model, core_number, min_coauthors_hindex, max_coauthors_hindex, pagerank):
-    vectors = data["text"].apply(lambda x: model.get_sentence_vector(x) if not pd.isnull(x) else model.get_sentence_vector("")).to_list()    
-    nb_data = np.array(data["nb_paper"].apply(lambda x: x if not pd.isnull(x) else 0).to_list()).reshape(-1, 1)
-    coauthors_hindex_data = np.array(data["mean_coauthors_hindex"].to_list()).reshape(-1, 1)
+def format_data(
+    data, model, core_number, min_coauthors_hindex, max_coauthors_hindex, pagerank
+):
+    vectors = (
+        data["text"]
+        .apply(
+            lambda x: model.get_sentence_vector(x)
+            if not pd.isnull(x)
+            else model.get_sentence_vector("")
+        )
+        .to_list()
+    )
+    nb_data = np.array(
+        data["nb_paper"].apply(lambda x: x if not pd.isnull(x) else 0).to_list()
+    ).reshape(-1, 1)
+    coauthors_hindex_data = np.array(data["mean_coauthors_hindex"].to_list()).reshape(
+        -1, 1
+    )
     n_coauthors_data = np.array(data["n_coauthors"].to_list()).reshape(-1, 1)
     core_number = np.array(core_number["core_number"].to_list()).reshape(-1, 1)
-    min_coauthors_hindex = np.array(min_coauthors_hindex["min_coauthor_hindex"].to_list()).reshape(-1, 1)
-    max_coauthors_hindex = np.array(max_coauthors_hindex["max_coauthor_hindex"].to_list()).reshape(-1, 1)
+    min_coauthors_hindex = np.array(
+        min_coauthors_hindex["min_coauthor_hindex"].to_list()
+    ).reshape(-1, 1)
+    max_coauthors_hindex = np.array(
+        max_coauthors_hindex["max_coauthor_hindex"].to_list()
+    ).reshape(-1, 1)
     pagerank = np.array(pagerank["pagerank"].to_list()).reshape(-1, 1)
-    
-    X = np.concatenate((vectors, nb_data, coauthors_hindex_data, n_coauthors_data, core_number, min_coauthors_hindex, max_coauthors_hindex, pagerank), axis=1)
+
+    X = np.concatenate(
+        (
+            vectors,
+            nb_data,
+            coauthors_hindex_data,
+            n_coauthors_data,
+            core_number,
+            min_coauthors_hindex,
+            max_coauthors_hindex,
+            pagerank,
+        ),
+        axis=1,
+    )
     y = np.array(data["hindex"].to_list())
-    
+
     return X, y
