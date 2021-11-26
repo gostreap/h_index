509596891942bd21827eadaf93a0fc95f08d808a
Christian Kotait <christian.kotait@gmail.com>
2021-11-26 23:46:13 +0100

    Gensim

---
+++ src/gensim_processing.py
@@ -0,0 +1,8 @@
+from gensim_utils import tokenize_abstracts,latentSemanticIndexing,similarityIndex,get_author_abstract_similarity
+
+dic, corp = tokenize_abstracts()
+
+num_topics = 250
+lsi = latentSemanticIndexing(dic,corp,num_topics)
+
+index = similarityIndex(lsi,corp,num_topics)
