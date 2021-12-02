356871b0f52f66b58d9cb5ad3c32e2d7e2e886b5
Tristan François <tpv.francois@gmail.com>
2021-12-02 12:14:00 +0100

    Updated submit.py

--- src/submit.py
+++ src/submit.py
@@ -1,15 +1,38 @@
 from read_data import get_test_data
-from fasttext_utils import get_submission_data, normalize
+from preprocess_utils import get_submission_data, normalize
+from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
 
 def submit(model):
     X_train, y_train, X_test, y_test = get_submission_data()
-    X_train, X_test = normalize(X_train, X_test)
+    # X_train, X_test = normalize(X_train, X_test)
 
     print(X_train.shape)
     print(X_test.shape)
 
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
+
+    test, _ = get_test_data()
+    test["hindex"] = y_pred
+    submission = test[["author", "hindex"]]
+    submission.to_csv("../tmp/submission.csv", index=None)
+
+def submit_mean():
+    model_lgbm = LGBMRegressor(n_estimators=4000)
+    model_cat = CatBoostRegressor(verbose=False, task_type="GPU", iterations=40000)
+
+    X_train, y_train, X_test, y_test = get_submission_data()
+
+    print(X_train.shape, X_test.shape)
+
+    model_cat.fit(X_train, y_train)
+    cat_pred = model_cat.predict(X_test)
+
+    model_lgbm.fit(X_train, y_train)
+    lgbm_pred = model_lgbm.predict(X_test)
+
+    y_pred = (lgbm_pred + cat_pred) / 2
 
     test, _ = get_test_data()
     test["hindex"] = y_pred
